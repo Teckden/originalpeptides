ab924e877c49dfac8057afc27ac62effced7b300
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-02-13 13:32:32 +0200

    sending email settings

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,9 @@
 class OrderMailer < ActionMailer::Base
-  default from: "notification@example.com"
+  default from: "notification@originalpeptides.com"
 
   def new_order(order)
     @order = order
-    @url = "http://example.com/orders"
-    mail(to: "", subject: "New order has been placed")
+    #@url = "http://originalpeptides.com.ua/orders"
+    mail(to: "originalpeptides@gmail.com", subject: "New order has been placed")
   end
 end
