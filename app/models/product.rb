4b6bf1acff76e22537b043865b9e01f70f78cd3c
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-02-13 09:51:38 +0200

    Problem with images (delete) solved

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@
 class Product < ActiveRecord::Base
   has_many :cart_products
   has_many :assets, dependent: :destroy
-  accepts_nested_attributes_for :assets
+  accepts_nested_attributes_for :assets, :allow_destroy => true
 
   attr_accessible :description, :price, :title, :available, :producer,
                         :assets_attributes, :custom_url
