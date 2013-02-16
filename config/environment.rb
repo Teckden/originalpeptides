71549afd180d6e70c83df6989017f9b86b0ba373
oricomua <oricomua@athena.hostfabrica.ru>
2013-02-16 13:10:06 +0400

    on selecet to false

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CartTemplate::Application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+   :address => "smtp.hostfabrica.ru",
+   :port => 25,
+   :domain => "originalpeptides.com.ua",
+   :authentication => "cram_md5",
+   :user_name => "noreply@originalpeptides.com.ua",
+   :password => "go_35-mail"
+}
+
