c0004ae1b529bf2f799fb7a87d8b80db763c4df2
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-02-16 09:44:45 +0200

    added: style for img_list, email message sent, scrolling in chrome, lightbox fix

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 class OrderMailer < ActionMailer::Base
   #default from: "noreply@originalpeptides.com.ua"
 
   def new_order(order)
     @order = order
-    #@url = "http://originalpeptides.com.ua/orders"
-    mail(to: "originalpeptides@gmail.com", subject: "New order has been placed", from: "noreply@originalpeptides.com.ua")
+    @url = "http://originalpeptides.com.ua/orders"
+    mail(to: "originalpeptides@gmail.com", subject: "На сайте оформлен новый заказ", from: "noreply@originalpeptides.com.ua")
   end
 end
