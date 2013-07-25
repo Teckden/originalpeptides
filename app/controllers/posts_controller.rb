b1b22e664305e953e0bdbc645df05fadcf660ef0
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-07-26 00:22:20 +0300

    CRUD for Blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class PostsController < ApplicationController
   layout 'blog'
   def index
@@ -5,20 +6,44 @@ class PostsController < ApplicationController
   end
 
   def show
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def update
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new()
   end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to post_path(@post), notice: 'Статья успешно добавлена в блог'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params[:post])
+      redirect_to post_path(@post), notice: 'Изменения успешно сохранены'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to posts_path, notice: 'Статья успешно удалена'
+    else
+      redirect_to post_path(@post), notice: 'К сожалению статья не была удалена. Свяжитесь с разроботчиком'
+    end
+  end
+
 end
