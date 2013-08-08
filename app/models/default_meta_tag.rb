b80e10652b0ddab7bd60068de1c5f5620a4e36de
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-08-08 20:35:08 +0300

    Refactored meta tags

--- app/models/default_meta_tag.rb
+++ app/models/default_meta_tag.rb
@@ -3,18 +3,4 @@ class DefaultMetaTag < ActiveRecord::Base
 
   validates :meta_description, :meta_keywords, uniqueness: true
 
-  def self.filter_tag_description(page_identifier)
-    begin
-      find_by_method("#{page_identifier}").meta_description
-    rescue
-    end
-  end
-
-  def self.filter_tag_keywords(page_identifier)
-    begin
-      find_by_method("#{page_identifier}").meta_keywords
-    rescue
-    end
-  end
-
 end
