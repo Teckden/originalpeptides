8d2520f75eceb68e1043ace8d447d220f8c9d1fc
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-08-12 14:41:09 +0300

    Pagination added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def index
     session[:blog_admin] = true if params[:blog_admin]
-    @posts = Post.all.reverse
+    @posts = Post.paginate(:page => params[:page], :per_page => 10).order("updated_at DESC")
   end
 
   def show
