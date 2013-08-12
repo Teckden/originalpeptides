6d600adb32daf59cc1e90df35da87254ef937b04
Denys Ivanchuk <denys.ivanchuk@gmail.com>
2013-08-12 16:10:07 +0400

    Gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422134417) do
+ActiveRecord::Schema.define(:version => 20130809172648) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "username"
@@ -79,6 +79,15 @@ ActiveRecord::Schema.define(:version => 20130422134417) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+    t.string   "meta_description"
+    t.string   "meta_keywords"
+  end
+
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
@@ -90,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20130422134417) do
     t.string   "custom_url"
     t.string   "meta_description"
     t.string   "meta_keywords"
+    t.decimal  "russian_price",    :precision => 8, :scale => 2
   end
 
 end
