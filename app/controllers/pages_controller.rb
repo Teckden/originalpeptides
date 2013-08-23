d1377f34a20600b7c0bba0cbbbb3e44a6a1c6a18
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-08-23 12:47:44 +0300

    Changed logic of the delivery details to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,18 @@
 # coding: utf-8
-class DeliveryDetailsController < ApplicationController
+class PagesController < ApplicationController
 
   before_filter :confirm_logged_in
 
   layout 'admin'
 
   def edit
-    @delivery_details = Page.find_by_page(params[:custom_url])
+    @page = Page.find_by_custom_url(params[:custom_url])
   end
 
   def update
-    @delivery_detail = Page.find_by_page(params[:custom_url])
+    @page = Page.find_by_custom_url(params[:custom_url])
 
-    if @delivery_detail.update_attributes(params[:page])
+    if @page.update_attributes(params[:page])
       redirect_to else_path, notice: "Страница успешно обновлена"
     else
       render action: "edit"
