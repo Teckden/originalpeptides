e46628c4dd134faeb7ee13051dc6fd3400dc5bfc
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-08-23 12:47:44 +0300

    Changed logic of the delivery details to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,8 @@
 class Page < ActiveRecord::Base
   attr_accessible :custom_url, :text, :page, :title
 
+  validates :custom_url, :title, :text, presence: true
+
   def self.get_custom_url(page)
     find_by_page(page.to_s).custom_url
   end
--- db/migrate/20121113145548_create_delivery_details.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeliveryDetails < ActiveRecord::Migration
-  def change
-    create_table :delivery_details do |t|
-      t.text :delivery
-
-      t.timestamps
-    end
-  end
-end
