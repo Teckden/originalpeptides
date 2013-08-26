2aea0c96b29f06b3d3db42dd604e64549a6381bb
Denys Ivanchuk <denys.ivanchuk@rafter.com>
2013-08-26 13:53:26 +0300

    Security settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CartTemplate::Application.config.secret_token = '2c213f2bcf62e97c5eebde8cc963bbb0d9c5fb365ee3045514ff6f1a7d6b6edd6e56750516f35ee3626eef12266d348ad926f4b6fd4105d2a4f6dbd51a2c309a'
+CartTemplate::Application.config.secret_token = CONFIG[:secret_token]
